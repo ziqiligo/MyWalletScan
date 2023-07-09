36ca97bc24813fb33a6dbb7b187ba9e1d8ad3e3c
ziqiligo <134369319+ziqiligo@users.noreply.github.com>
2023-07-09 15:48:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
 
 export default defineConfig({
+    version: "20230709",
     plugins: [react()],
     resolve: {
         alias: {
